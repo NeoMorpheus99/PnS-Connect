7db561e6251f860f2c99f2a6c4ac986d837b8d39
Genius <Velociraptor.rex@hotmail.com>
2024-05-30 11:20:17 +0500

    Add login and registration system Add helpers.py and sql.py to help with displaying info and querying db respectively Add gitignore to ignore flask_session & __pycache__ Add apology.html to display apology

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, app, request, render_template, session, redirect
+from flask_session import Session
+from helpers import login_required, apology
+from werkzeug.security import check_password_hash, generate_password_hash
+from sql import SQL
+
+app = Flask(__name__)
+app.debug = True
+
+db = SQL("sqlite:///connect.db")
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get(
+                "username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["pass"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirmation")
+
+        if not confirm or not password or not username:
+            return apology("An input field was left blank!")
+        if not confirm == password:
+            return apology("Sorry! The passwords don't match.", 400)
+
+        try:
+            db.execute("INSERT INTO users (username, pass) VALUES (?, ?)",
+                       username, generate_password_hash(password))
+        except ValueError:
+            return apology("The Username was already taken!", 400)
+
+        return redirect("/")
